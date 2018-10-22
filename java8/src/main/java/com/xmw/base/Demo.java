484b992da822681ac5293f5811aa62c1f3eb63ef
mingwei.xia <mingwei.xia@56qq.com>
2018-10-22 11:20:29 +0800

    java bytecode study

---
+++ java8/src/main/java/com/xmw/base/Demo.java
@@ -0,0 +1,20 @@
+package com.xmw.base;
+
+/**
+ * bytecode study
+ * https://www.jianshu.com/p/252f381a6bc4
+ *
+ * @author mingwei.xia
+ * @date 2018/10/22 9:36
+ * @since V1.0
+ */
+public class Demo {
+    public String bbbbbb = "ccccc";
+    private int abcde = 112312;
+
+    public void test(String name, int xixi) {
+        String mmmmmm = "xxxxxx";
+        String test = "test123";
+        String xmw = "xiamingwei";
+    }
+}
---
+++ java8/src/main/java/com/xmw/base/DemoInterface.java
@@ -0,0 +1,15 @@
+package com.xmw.base;
+
+/**
+ * bytecode study
+ *
+ * @author mingwei.xia
+ * @date 2018/10/22 11:02
+ * @since V1.0
+ */
+public interface DemoInterface {
+
+    String demo = "xiamingwei";
+
+    Object sayHello(String hello, int aaa);
+}
